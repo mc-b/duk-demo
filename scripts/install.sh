bf09466d4b560036bd9ce56274a27f7128f3866e
mc-b <marcel.bernet@ch-open.ch>
2019-12-25 17:16:38 +0100

    Obsolet ist in Notebook work/demo/

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,41 +7,6 @@
 cp -rpv data/* /data/
 
 ##########################
-# User snoopy anlegen
-
-openssl genrsa -out snoopy.pem 2048
-openssl req -new -key snoopy.pem -out snoopy.csr -subj "/CN=snoopy"
-
-cat <<%EOF% | kubectl apply -f -
-apiVersion: certificates.k8s.io/v1beta1
-kind: CertificateSigningRequest
-metadata:
-  name: user-request-snoopy
-spec:
-  groups:
-  - system:authenticated
-  request: $(cat snoopy.csr | base64 | tr -d '\n')
-  usages:
-  - digital signature
-  - key encipherment
-  - client auth
-%EOF%
-
-kubectl certificate approve user-request-snoopy
-kubectl get csr user-request-snoopy -o jsonpath='{.status.certificate}' | base64 -d > snoopy.crt
-
-mkdir .kube
-kubectl --kubeconfig .kube/config-snoopy config set-cluster kubernetes --insecure-skip-tls-verify=true --server=https://$(hostname -I | cut -d ' ' -f 2):6443
-kubectl --kubeconfig .kube/config-snoopy config set-credentials snoopy --client-certificate=snoopy.crt --client-key=snoopy.pem --embed-certs=true
-
-kubectl --kubeconfig .kube/config-snoopy config set-context snoopy --cluster=kubernetes --user=snoopy 
-kubectl --kubeconfig .kube/config-snoopy config use-context snoopy
-
-kubectl apply -f rbac/
-
-kubectl describe secret $(kubectl get secret | grep snoopy | awk '{print $1}') >dashboard-snoopy.txt
-
-##########################
 # HELM (Achtung bei Versionwechsel auch client.sh aendern).
 curl -L https://git.io/get_helm.sh | bash
 kubectl create serviceaccount --namespace kube-system tiller
