8bbfdadecbe2d053846b7020155c998f5a9a8bcd
mc-b <marcel.bernet@ch-open.ch>
2020-03-06 17:07:10 +0100

    Logging erst spaeter starten, sonst werden die Logs zu gross

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,18 +22,3 @@ helm install prometheus-operator stable/prometheus-operator --namespace=monitori
 curl -s -L https://git.io/getLatestIstio | ISTIO_VERSION=1.3.3 sh -
 for i in istio-1.3.3/install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
 kubectl apply -f istio-1.3.3/install/kubernetes/istio-demo.yaml
-
-##########################
-# Logging (ElasticSearch, Kibana, Fluent-bit)
-
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/all-in-one.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/elasticsearch.yaml
-
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/kibana.yaml
-
-kubectl create namespace logging
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/fluent-bit-service-account.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/fluent-bit-role.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/fluent-bit-role-binding.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/fluent-bit-configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/logging/fluent-bit-ds.yaml  
