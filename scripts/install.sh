00e385b7f40b3c5107286de9d1737d8ee0ccb9f0
mc-b <marcel.bernet@ch-open.ch>
2020-11-02 15:43:42 +0100

    Helm Repo aktualisiert

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ cp -rpv data/* /data/
 ##########################
 # Monitoring (Prometheus)
 kubectl create namespace monitoring
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add stable https://charts.helm.sh/stable
 helm install prometheus-operator stable/prometheus-operator --namespace=monitoring
 
 ##########################
